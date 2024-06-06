c922163f432cf8fd743c3c8959d2f5dadce50922
camiSebe <csebellin@fi.uba.ar>
2024-06-06 17:46:16 -0300

    Add: Se agrega uso del toggle, para q solo se envie cuando el cliente apreta y/o suelta la tecla q genere un comando

--- include/server_src/game/game_comandos.h
+++ include/server_src/game/game_comandos.h
@@ -6,52 +6,53 @@
 class Comando {
 public:
     uint16_t client_id;
-    bool toggle = false;
-    virtual bool ejecutar(Game& game) = 0;
+    bool toggle;
+    virtual void ejecutar(Game& game) = 0;
     virtual ~Comando() {}
 
-    void set_client_id(uint16_t client_id) { this->client_id = client_id; }
-    void set_toggle(bool toggle) { this->toggle = toggle; }
+    Comando();
+    void set_client_id(uint16_t client_id);
+    void set_toggle(bool toggle);
 };
 
 class Disparar: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 class MoverDerecha: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 class MoverDerechaRapido: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 class MoverIzquierda: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 class MoverIzquierdaRapido: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 class MoverArriba: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 class MoverAbajo: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 class Saltar: public Comando {
 public:
-    bool ejecutar(Game& game) override;
+    void ejecutar(Game& game) override;
 };
 
 #endif
--- src/server_src/game/game_comandos.cpp
+++ src/server_src/game/game_comandos.cpp
@@ -2,21 +2,57 @@
 
 #include "../../include/server_src/game/game_class.h"
 
-bool Disparar::ejecutar(Game& game) { return game.atacar_enemigo(client_id); }
+Comando::Comando(): toggle(false) {}
 
-bool MoverDerecha::ejecutar(Game& game) { return game.mover("derecha", client_id); }
+void Comando::set_client_id(uint16_t client_id) { this->client_id = client_id; }
 
-bool MoverDerechaRapido::ejecutar(Game& game) { return game.mover("derecha_rapido", client_id); }
+void Comando::set_toggle(bool toggle) { this->toggle = toggle; }
 
-bool MoverIzquierda::ejecutar(Game& game) { return game.mover("izquierda", client_id); }
-
-bool MoverIzquierdaRapido::ejecutar(Game& game) {
-    return game.mover("izquierda_rapido", client_id);
+void Disparar::ejecutar(Game& game) {
+    while (toggle) {
+        game.atacar_enemigo(client_id);
+    }
 }
 
-bool Saltar::ejecutar(Game& game) { return game.mover("saltar", client_id); }
+void MoverDerecha::ejecutar(Game& game) {
+    while (toggle) {
+        game.mover("derecha", client_id);
+    }
+}
+
+void MoverDerechaRapido::ejecutar(Game& game) {
+    while (toggle) {
+        game.mover("derecha_rapido", client_id);
+    }
+}
+
+void MoverIzquierda::ejecutar(Game& game) {
+    while (toggle) {
+        game.mover("izquierda", client_id);
+    }
+}
+
+void MoverIzquierdaRapido::ejecutar(Game& game) {
+    while (toggle) {
+        game.mover("izquierda_rapido", client_id);
+    }
+}
+
+void Saltar::ejecutar(Game& game) {
+    while (toggle) {
+        game.mover("saltar", client_id);
+    }
+}
 
 // Arriba y abajo creo que no hay, sería saltar y agacharse?
-bool MoverArriba::ejecutar(Game& game) { return game.mover("arriba", client_id); }
+void MoverArriba::ejecutar(Game& game) {
+    while (toggle) {
+        game.mover("arriba", client_id);
+    }
+}
 
-bool MoverAbajo::ejecutar(Game& game) { return game.mover("abajo", client_id); }
+void MoverAbajo::ejecutar(Game& game) {
+    while (toggle) {
+        game.mover("abajo", client_id);
+    }
+}
