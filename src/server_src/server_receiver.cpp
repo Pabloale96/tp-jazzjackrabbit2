5f4a4f06892841933b34b516a3a63b76f46c87b6
Pablo Martin Alejandro <pabloale96@gmail.com>
2024-06-06 18:38:43 -0300

    FIX: resuelvo merge

--- src/server_src/server_receiver.cpp
+++ src/server_src/server_receiver.cpp
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-#include "../../include/server_src/server_receiver.h"
-
-#include <memory>
-#include <string>
-#include <utility>  // move()
-#include <vector>
-
-#include "../../include/common_src/liberror.h"
-#include "../../include/server_src/game/gameloop_class.h"
-#include "../../include/server_src/server_protocol.h"
-
-ServerReceiver::ServerReceiver(ProtocolServer& protocolo_server, bool& was_closed,
-                               GameloopMonitor& gameloop_monitor, uint16_t gameloop_id,
-                               uint16_t cliente_id):
-        protocolo_server(protocolo_server),
-        was_closed(was_closed),
-        gameloop_monitor(gameloop_monitor),
-        gameloop_id(gameloop_id),
-        cliente_id(cliente_id),
-        client_commands(gameloop_monitor.obtener_queue_de_client_commands(gameloop_id)) {}
-
-void ServerReceiver::run() {
-    while (!was_closed) {
-        try {
-            auto comando = protocolo_server.recibir_acciones(was_closed, cliente_id);
-            client_commands.push(std::shared_ptr<Comando>(
-                    std::move(comando)));  // Como use unique_ptr por la herencia, tengo q
-                                           // convertirlo a shared_ptr
-            if (!this->is_alive()) {
-                break;
-            }
-        } catch (const LibError& err) {
-            // TODO: gameloop, cerra este cliente/personaje
-            //gameloop_monitor.borrar_cliente_de_gameloop(gameloop_id, cliente_id);
-            break;
-        }
-    }
-    std::cout << "Cerrando socket del cliente" << cliente_id << std::endl;
-    gameloop_monitor.borrar_cliente_de_gameloop(gameloop_id, cliente_id);
-    protocolo_server.cerrar_socket_cliente();
-}
-
-ServerReceiver::~ServerReceiver() {}
-=======
 #include "../../include/server_src/server_receiver.h"
 
 #include <memory>
@@ -86,4 +41,3 @@ void ServerReceiver::run() {
 }
 
 ServerReceiver::~ServerReceiver() {}
->>>>>>> main
