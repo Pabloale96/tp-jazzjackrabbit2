9343542b8b15fb3d23c7eb6024f6b46b0584129b
camiSebe <csebellin@fi.uba.ar>
2024-06-15 20:30:45 -0300

    Add: Se agregan los enemigos en los lugares designados por el diseño del juego

--- src/server_src/game/game_enemigo.cpp
+++ src/server_src/game/game_enemigo.cpp
@@ -5,19 +5,10 @@
 #include <arpa/inet.h>
 
 #include "defines_msg.h"
-#define ITERACIONES_PARA_REVIVIR 15
-#define TIME_REVIVE_INICIAL 50
+#include "../../include/server_src/yaml_config.h"
 
 Enemigo::Enemigo(uint16_t id_enemigo):
-        id_enemigo(id_enemigo),
-        iteraciones(0)
-// cppcheck-suppress readability/multiline_comment
-/*
-time_revive(TIME_REVIVE_INICIAL),
-prob_municion(0.0f),
-prob_vida(0.0f)
-*/
-{}
+        id_enemigo(id_enemigo) {}
 
 Enemigo::Enemigo(uint16_t* datos_enemigo):
         id_enemigo(ntohs(datos_enemigo[POS_ID_ENEMIGO])),
@@ -26,20 +17,35 @@ Enemigo::Enemigo(uint16_t* datos_enemigo):
                          ntohs(datos_enemigo[POS_POSY_ENEMIGO])) {}
 
 void Enemigo::set_enemigo_id(uint16_t id_enemigo) { this->id_enemigo = id_enemigo; }
+
 void Enemigo::set_tipo_enemigo(TipoEnemigo tipo_enemigo) { this->tipo_enemigo = tipo_enemigo; }
+
 void Enemigo::set_posicion_enemigo(uint16_t x, uint16_t y) { posicion_enemigo.set_posicion(x, y); }
+
 void Enemigo::set_vidas(uint16_t vidas) { this->vidas = vidas; }
+
 void Enemigo::set_danio_al_jugador(uint16_t danio_al_jugador) {
     this->danio_al_jugador = danio_al_jugador;
 }
+
 void Enemigo::set_puntos(uint16_t puntos) { this->puntos = puntos; }
 
+void Enemigo::set_time_revive(int time_revive) { this->time_revive = time_revive; }
+
+void Enemigo::set_prob_municion(float prob_municion) { this->prob_municion = prob_municion; }
+
+void Enemigo::set_prob_vida(float prob_vida) { this->prob_vida = prob_vida; }
 
 uint16_t Enemigo::get_id_enemigo() const { return id_enemigo; }
+
 TipoEnemigo Enemigo::get_tipo_enemigo() const { return tipo_enemigo; }
+
 Posicion Enemigo::get_posicion_enemigo() const { return posicion_enemigo; }
+
 uint16_t Enemigo::get_vidas() const { return vidas; }
+
 uint16_t Enemigo::get_danio_al_jugador() const { return danio_al_jugador; }
+
 uint16_t Enemigo::get_puntos() const { return puntos; }
 
 void Enemigo::actualizar() {
@@ -56,6 +62,7 @@ void Enemigo::recibir_disparo(uint8_t danio) {
     }
 }
 
+/*
 void Enemigo::aumentar_iteraciones() {
     if (iteraciones < ITERACIONES_PARA_REVIVIR) {
         iteraciones++;
@@ -66,6 +73,7 @@ void Enemigo::aumentar_iteraciones() {
         iteraciones = 0;
     }
 }
+*/
 
 void Enemigo::matar() { vidas = 0; }
 
@@ -80,67 +88,59 @@ bool Enemigo::esta_vivo() {
 
 
 // ************  ENEMIGO 1 ("nombre")  ************
-#define POS_x_INICIAL_ENEMIGO_1 5
-#define POS_y_INICIAL_ENEMIGO_1 1
-#define VIDAS_ENEMIGO_1 1
-#define DANIO_ENEMIGO_1 1
-#define PUNTOS_ENEMIGO_1 1
-
 // Enemigo1 constructor
-Enemigo1::Enemigo1(uint16_t id): Enemigo(id) {
+Enemigo1::Enemigo1(uint16_t id, uint16_t x, uint16_t y): Enemigo(id) {
     set_tipo_enemigo(TipoEnemigo::ENEMIGO1);
-    set_posicion_enemigo(POS_x_INICIAL_ENEMIGO_1, POS_y_INICIAL_ENEMIGO_1);
-    set_vidas(VIDAS_ENEMIGO_1);
-    set_danio_al_jugador(DANIO_ENEMIGO_1);
-    set_puntos(PUNTOS_ENEMIGO_1);
+    set_posicion_enemigo(x, y);
+    set_vidas(YAMLConfig::getConfig().enemigo_1.vidas);
+    set_danio_al_jugador(YAMLConfig::getConfig().enemigo_1.danio);
+    set_puntos(YAMLConfig::getConfig().enemigo_1.puntos);
+    set_time_revive(YAMLConfig::getConfig().enemigo_1.segundos_para_revivir);
+    set_prob_municion(YAMLConfig::getConfig().enemigo_1.proba_droppear_municion);
+    set_prob_vida(YAMLConfig::getConfig().enemigo_1.proba_droppear_vida);
+
 }
 
 void Enemigo1::revivir_enemigo() {
     if (!esta_vivo()) {
-        set_vidas(VIDAS_ENEMIGO_1);
+        set_vidas(YAMLConfig::getConfig().enemigo_1.vidas);
     }
 }
 
 // ************  ENEMIGO 2 ("nombre")  ************
-#define POS_x_INICIAL_ENEMIGO_2 15
-#define POS_y_INICIAL_ENEMIGO_2 1
-#define VIDAS_ENEMIGO_2 2
-#define DANIO_ENEMIGO_2 2
-#define PUNTOS_ENEMIGO_2 2
-
 // Enemigo2 constructor
-Enemigo2::Enemigo2(uint16_t id): Enemigo(id) {
+Enemigo2::Enemigo2(uint16_t id, uint16_t x, uint16_t y): Enemigo(id) {
     set_tipo_enemigo(TipoEnemigo::ENEMIGO2);
-    set_posicion_enemigo(POS_x_INICIAL_ENEMIGO_2, POS_y_INICIAL_ENEMIGO_2);
-    set_vidas(VIDAS_ENEMIGO_2);
-    set_danio_al_jugador(DANIO_ENEMIGO_2);
-    set_puntos(PUNTOS_ENEMIGO_2);
+    set_posicion_enemigo(x, y);
+    set_vidas(YAMLConfig::getConfig().enemigo_2.vidas);
+    set_danio_al_jugador(YAMLConfig::getConfig().enemigo_2.danio);
+    set_puntos(YAMLConfig::getConfig().enemigo_2.puntos);
+    set_time_revive(YAMLConfig::getConfig().enemigo_2.segundos_para_revivir);
+    set_prob_municion(YAMLConfig::getConfig().enemigo_2.proba_droppear_municion);
+    set_prob_vida(YAMLConfig::getConfig().enemigo_2.proba_droppear_vida);
 }
 
 void Enemigo2::revivir_enemigo() {
     if (!esta_vivo()) {
-        set_vidas(VIDAS_ENEMIGO_2);
+        set_vidas(YAMLConfig::getConfig().enemigo_2.vidas);
     }
 }
 
-// ************  ENEMIGO 3 ("nombre")  ************
-#define POS_x_INICIAL_ENEMIGO_3 30
-#define POS_y_INICIAL_ENEMIGO_3 5  // volador
-#define VIDAS_ENEMIGO_3 3
-#define DANIO_ENEMIGO_3 3
-#define PUNTOS_ENEMIGO_3 3
-
+// ************  ENEMIGO 3 ("VOLADOR")  ************
 // Enemigo3 constructor
-Enemigo3::Enemigo3(uint16_t id): Enemigo(id) {
+Enemigo3::Enemigo3(uint16_t id, uint16_t x, uint16_t y): Enemigo(id) {
     set_tipo_enemigo(TipoEnemigo::ENEMIGO3);
-    set_posicion_enemigo(POS_x_INICIAL_ENEMIGO_3, POS_y_INICIAL_ENEMIGO_3);
-    set_vidas(VIDAS_ENEMIGO_3);
-    set_danio_al_jugador(DANIO_ENEMIGO_3);
-    set_puntos(PUNTOS_ENEMIGO_3);
+    set_posicion_enemigo(x, y);
+    set_vidas(YAMLConfig::getConfig().enemigo_3.vidas);
+    set_danio_al_jugador(YAMLConfig::getConfig().enemigo_3.danio);
+    set_puntos(YAMLConfig::getConfig().enemigo_3.puntos);
+    set_time_revive(YAMLConfig::getConfig().enemigo_3.segundos_para_revivir);
+    set_prob_municion(YAMLConfig::getConfig().enemigo_3.proba_droppear_municion);
+    set_prob_vida(YAMLConfig::getConfig().enemigo_3.proba_droppear_vida);
 }
 
 void Enemigo3::revivir_enemigo() {
     if (!esta_vivo()) {
-        set_vidas(VIDAS_ENEMIGO_3);
+        set_vidas(YAMLConfig::getConfig().enemigo_3.vidas);
     }
 }
