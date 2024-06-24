10629451350c104d72f566b6dc06a96e9c62c898
camiSebe <csebellin@fi.uba.ar>
2024-06-24 19:04:59 -0300

    Add: Mas tests

--- tests/protocol_server_test.cpp
+++ tests/protocol_server_test.cpp
@@ -4,6 +4,7 @@
 #include "../include/server_src/server_protocol.h"
 
 // Test cases
+/*
 TEST(ProtocolServerTest, TestEnvioIdCliente) {
     Socket server("8080");
     Socket cliente = server.accept();
@@ -12,7 +13,7 @@ TEST(ProtocolServerTest, TestEnvioIdCliente) {
     uint16_t id_cliente = 12345;
 
     protocolServer.enviar_id_jugador(id_cliente, was_closed);
-    ASSERT_EQ(false, false);
+    ASSERT_EQ(was_closed, false);
 }
 
 
@@ -68,18 +69,94 @@ TEST(ProtocolServerTest, TestRecibirNombrePartida) {
     ASSERT_EQ(nombre_recibido, "partida1");
 }
 
-
-/*
-TEST(ProtocolServerTest, TestEnviarEscenario) {
+TEST(ProtocolServerTest, TestEnviarPlataformaHorizontal) {
     Socket server("8086");
     Socket cliente = server.accept();
     ProtocolServer protocolServer(std::move(cliente));
     bool was_closed = false;
-    Game game(1, 1, 1);
-    protocolServer.enviar_escenario(game, was_closed);
+    uint16_t x = 0;
+    uint16_t y = 0;
+    uint16_t w = 15;
+    uint16_t h = 1;
+    platform type = platform::HORIZONTAL;
+    bool flip = false;
+    Platform plataforma_horizontal(x, y, w, h, (uint16_t) type, flip);
+    std::vector<Platform> escenario;
+    escenario.push_back(plataforma_horizontal);
+    protocolServer.enviar_escenario(escenario, was_closed);
+    ASSERT_EQ(was_closed, false);
+}
+
+TEST(ProtocolServerTest, TestEnviarPlataformaVertical) {
+    Socket server("8087");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    uint16_t x = 3;
+    uint16_t y = 3;
+    uint16_t w = 1;
+    uint16_t h = 3;
+    platform type = platform::VERTICAL;
+    bool flip = false;
+    Platform plataforma_horizontal(x, y, w, h, (uint16_t) type, flip);
+    std::vector<Platform> escenario;
+    escenario.push_back(plataforma_horizontal);
+    protocolServer.enviar_escenario(escenario, was_closed);
+    ASSERT_EQ(was_closed, false);
+}
+
+TEST(ProtocolServerTest, TestConfirmarFinLobby) {
+    Socket server("8088");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    bool confirmacion = protocolServer.confirmar_fin_lobby(was_closed);
+    ASSERT_EQ(confirmacion, true);
+}
+
+TEST(ProtocolServerTest, TestUnirseAPartida) {
+    Socket server("8089");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    uint8_t buffer = protocolServer.crear_partida(was_closed);
+    ASSERT_EQ(buffer, UNIRSE_A_PARTIDA);
+}
+
+TEST(ProtocolServerTest, TestEnviarPartidasDisponiblesVacioLlamaACrearPartida) {
+    Socket server("8089");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    GameloopMonitor gameloop_monitor;
+    uint8_t buffer = protocolServer.enviar_partidas_disponibles(gameloop_monitor, was_closed);
+    ASSERT_EQ(buffer, CREAR_PARTIDA);
+}
+
+TEST(ProtocolServerTest, TestEnviarPartidasDisponibles) {
+    Socket server("8089");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    GameloopMonitor gameloop_monitor;
+    gameloop_monitor.crear_gameloop("partida1", 1, (uint8_t)personajes::JAZZ);
+    uint8_t buffer = protocolServer.enviar_partidas_disponibles(gameloop_monitor, was_closed);
+    ASSERT_EQ(buffer, UNIRSE_A_PARTIDA);
 }
 */
 
+TEST(ProtocolServerTest, TestEnviarMuchasPartidasDisponibles) {
+    Socket server("8089");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    GameloopMonitor gameloop_monitor;
+    gameloop_monitor.crear_gameloop("partida1", 1, (uint8_t)personajes::JAZZ);
+    gameloop_monitor.crear_gameloop("partida2", 2, (uint8_t)personajes::SPAZZ);
+    gameloop_monitor.crear_gameloop("partida3", 3, (uint8_t)personajes::LORI);
+    uint8_t buffer = protocolServer.enviar_partidas_disponibles(gameloop_monitor, was_closed);
+    ASSERT_EQ(buffer, UNIRSE_A_PARTIDA);
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
--- tests/test_consola_cliente.cpp
+++ tests/test_consola_cliente.cpp
@@ -8,6 +8,8 @@ ProtocolClient crearProtocoloCliente(const std::string& hostname, const std::str
 }
 
 // Test cases
+
+/*
 TEST(ProtocolClientTest, TestRecibirIdJugador) {
     ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.1", "8080");
     uint16_t id_jugador = protocolClient.recibir_id_jugador();
@@ -49,16 +51,73 @@ TEST(ProtocolClientTest, TestCrearPartida) {
     ASSERT_EQ(se_envio_nombre, true);
 }
 
-
-/*
-TEST(ProtocolClientTest, TestRecibirEscenario) {
-    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.7'", "8086");
-    GameStateClient gamestate;
-    bool se_recibio_escenario = protocolClient.recibir_escenario(gamestate);
+TEST(ProtocolClientTest, TestRecibirPlataformaHorizontal) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.7", "8086");
+    // Como necesito textura par el gamestate, creo una dummy (y todo lo que necesita)
+    SDL2pp::Window window("Dummy Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_HIDDEN);
+    SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+    ClaseTexturas dummyTexturas(renderer);
+    GameStateClient gamestate_test(dummyTexturas);
+    bool se_recibio_escenario = protocolClient.recibir_escenario(gamestate_test);
     ASSERT_EQ(se_recibio_escenario, true);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().size(), 1);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getX(), 0);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getY(), 0);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getW(), 15);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getH(), 1);
+}
+
+TEST(ProtocolClientTest, TestRecibirPlataformaVertical) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.8", "8087");
+    // Como necesito textura par el gamestate, creo una dummy (y todo lo que necesita)
+    SDL2pp::Window window("Dummy Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_HIDDEN);
+    SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+    ClaseTexturas dummyTexturas(renderer);
+    GameStateClient gamestate_test(dummyTexturas);
+    bool se_recibio_escenario = protocolClient.recibir_escenario(gamestate_test);
+    ASSERT_EQ(se_recibio_escenario, true);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().size(), 1);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getX(), 3);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getY(), 3);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getW(), 1);
+    ASSERT_EQ(gamestate_test.obtener_plataformas().at(0).getH(), 3);
+}
+
+TEST(ProtocolClientTest, TestConfirmarFinLobby) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.9", "8088");
+    bool confirmacion = protocolClient.confirmar_fin_lobby();
+    ASSERT_EQ(confirmacion, true);
+}
+
+TEST(ProtocolClientTest, TestUnirseAPartida) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.9", "8089");
+    bool se_unio_a_partida = protocolClient.unirse_a_partida();
+    ASSERT_EQ(se_unio_a_partida, true);
+}
+
+TEST(ProtocolClientTest, TestRecibirPartidasDisponiblesVacio) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.9", "8089");
+    std::map<uint16_t, std::string> partidas_disponibles;
+    protocolClient.recibir_partidas_disponibles(partidas_disponibles);
+    ASSERT_EQ(partidas_disponibles.size(), 0);
+}
+
+TEST(ProtocolClientTest, TestRecibirPartidasDisponibles) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.9", "8089");
+    std::map<uint16_t, std::string> partidas_disponibles;
+    protocolClient.recibir_partidas_disponibles(partidas_disponibles);
+    ASSERT_EQ(partidas_disponibles.size(), 1);
+    ASSERT_NE(partidas_disponibles.find(1), partidas_disponibles.end()) << "No se encontró el ID 1 en partidas_disponibles";
+    ASSERT_EQ(partidas_disponibles[1], "partida1");
 }
 */
 
+TEST(ProtocolClientTest, TestRecibirMuchasPartidasDisponibles) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.9", "8089");
+    std::map<uint16_t, std::string> partidas_disponibles;
+    protocolClient.recibir_partidas_disponibles(partidas_disponibles);
+    ASSERT_EQ(partidas_disponibles.size(), 3);
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
