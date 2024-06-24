f16cef1863c3484d45386ca72c66646aa45210cf
camiSebe <csebellin@fi.uba.ar>
2024-06-23 23:27:32 -0300

    Add: Se aregan mas tests

--- tests/protocol_server_test.cpp
+++ tests/protocol_server_test.cpp
@@ -16,9 +16,59 @@ TEST(ProtocolServerTest, TestEnvioIdCliente) {
 }
 
 
+TEST(ProtocolServerTest, TestRecibirPersonajeJazz) {
+    Socket server("8081");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+
+    uint8_t buffer = protocolServer.recibir_personaje(was_closed);
+    ASSERT_EQ((uint8_t) personajes::JAZZ, buffer);
+}
+
+
+TEST(ProtocolServerTest, TestRecibirPersonajeSpazz) {
+    Socket server("8082");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+
+    uint8_t buffer = protocolServer.recibir_personaje(was_closed);
+    ASSERT_EQ((uint8_t) personajes::SPAZZ, buffer);
+}
+
+TEST(ProtocolServerTest, TestRecibirPersonajeLori) {
+    Socket server("8083");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+
+    uint8_t buffer = protocolServer.recibir_personaje(was_closed);
+    ASSERT_EQ((uint8_t) personajes::LORI, buffer);
+}
+
+
+TEST(ProtocolServerTest, TestRecibirCodigoCrearPartida) {
+    Socket server("8084");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    uint8_t buffer = protocolServer.crear_partida(was_closed);
+    ASSERT_EQ(CREAR_PARTIDA, buffer);
+}
+
+
+TEST(ProtocolServerTest, TestRecibirNombrePartida) {
+    Socket server("8085");
+    Socket cliente = server.accept();
+    ProtocolServer protocolServer(std::move(cliente));
+    bool was_closed = false;
+    std::string nombre_recibido;
+    protocolServer.recibir_nombre_partida(nombre_recibido, was_closed);
+    ASSERT_EQ(nombre_recibido, "partida1");
+}
+
 int main(int argc, char** argv) {
    ::testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
-
-    // return 0;
 }
--- tests/test_consola_cliente.cpp
+++ tests/test_consola_cliente.cpp
@@ -3,21 +3,53 @@
 #include "../include/client_src/client_protocol.h"
 #include "../include/common_src/catedra/sockets.h"
 
-ProtocolClient crearProtocoloCliente() {
-    std::string hostname = "localhost";
-    std::string servicio = "8080";
+ProtocolClient crearProtocoloCliente(std::string hostname, std::string servicio) {
     return ProtocolClient(hostname, servicio);
 }
 
 // Test cases
 TEST(ProtocolClientTest, TestRecibirIdJugador) {
-    ProtocolClient protocolClient = crearProtocoloCliente();
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.1", "8080");
     uint16_t id_jugador = protocolClient.recibir_id_jugador();
     ASSERT_EQ(id_jugador, 12345);
+}
+
+
+TEST(ProtocolClientTest, TestEnviarPersonajeJazz) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.2", "8081");
+    bool se_envio_personaje = protocolClient.enviar_personaje("j");
+    ASSERT_EQ(se_envio_personaje, true);
+}
+
+
+TEST(ProtocolClientTest, TestEnviarPersonajeSpazz) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.3", "8082");
+    bool se_envio_personaje = protocolClient.enviar_personaje("s");
+    ASSERT_EQ(se_envio_personaje, true);
+}
+
+TEST(ProtocolClientTest, TestEnviarPersonajeLori) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.4", "8083");
+    bool se_envio_personaje = protocolClient.enviar_personaje("l");
+    ASSERT_EQ(se_envio_personaje, true);
+}
+
+
+TEST(ProtocolClientTest, TestEnviarCodigoCrearPartida) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.5", "8084");
+    bool se_envio_codigo = protocolClient.enviar_codigo_de_crear_partida();
+    ASSERT_EQ(se_envio_codigo, true);
+}
+
+
+TEST(ProtocolClientTest, TestCrearPartida) {
+    ProtocolClient protocolClient = crearProtocoloCliente("127.0.0.6", "8085");
+    std::string nombre_partida = "partida1";
+    bool se_envio_nombre = protocolClient.crear_partida(nombre_partida);
+    ASSERT_EQ(se_envio_nombre, true);
 }
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-    //return 0;
 }
